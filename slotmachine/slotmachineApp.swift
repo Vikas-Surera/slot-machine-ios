2c17d00756ac79a0f51dd9e8512a96cd587e9180
Vikas Surera <vikassurera@gmail.com>
2022-07-09 12:10:27 +0530

    Initial Commit

---
+++ slotmachine/slotmachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotmachineApp.swift
+//  slotmachine
+//
+//  Created by Vikas Surera on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct slotmachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
